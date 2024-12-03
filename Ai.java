1b438187f766a782d1e3a2f8459d56a6cc8a445d
prassu56 <prasuprasanna236@gmail.com>
2024-12-03 18:51:05 +0530

    Add files via upload

---
+++ Ai.java
@@ -0,0 +1,10 @@
+import java.io.PrintWriter;
+public class Ai{
+    public static void main(String[]args){
+        PrintWriter pw=new PrintWriter(System.out);
+        pw.println("hello world!");
+        pw.print("this is a");
+        pw.print("print statement.");
+        pw.close();
+    }
+}
