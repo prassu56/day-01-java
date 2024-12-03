90559e0fefc7cc538fdbfa556d2993e89c2b0d00
prassu56 <prasuprasanna236@gmail.com>
2024-12-03 19:52:59 +0530

    Add files via upload

---
+++ Concat.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Concat{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        System.out.println("congratulations"+a+"you won the 5lakhs");
+    }
+}
